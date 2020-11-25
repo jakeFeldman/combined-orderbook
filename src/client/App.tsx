ec3ecd50de804bcc62c168bb53e7abd9bcc8043e
jakeFeldman <jacob.h.feldman@gmail.com>
2020-11-25 02:12:46 -0700

    setup for heroku deploys

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,5 @@
 import MuiAppBar from '@material-ui/core/AppBar';
+import MuiToolbar from '@material-ui/core/Toolbar';
 import React from 'react';
 
 import { Orderbook } from './components/Orderbook/Orderbook';
@@ -6,7 +7,11 @@ import { Orderbook } from './components/Orderbook/Orderbook';
 export const App = (): JSX.Element => {
     return (
         <div>
-            <MuiAppBar position="static" />
+            <MuiAppBar position="static">
+                <MuiToolbar variant="dense">
+                    <h3>Bittrex | Poloniex Orderbook</h3>
+                </MuiToolbar>
+            </MuiAppBar>
             <Orderbook />
         </div>
     );
